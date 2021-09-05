9e76b70f6ef877919d5f095dadbaabe81797368a
Shouvik Bajpayee <bajpayeeshouvik@gmail.com>
2021-09-05 14:45:00 +0530

    go lang commit

---
+++ Booleans and Conditionals/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	age := 45
+
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("Age is less than 30")
+	} else if age < 40 {
+		fmt.Println("Age is less than 40")
+	} else {
+		fmt.Println("Age is not less than 45")
+	}
+
+	names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
+
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("Continuing at pos", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("Breaking at pos", index)
+			break
+		}
+		fmt.Printf("The value at pos %v is %v\n", index, value)
+	}
+
+}

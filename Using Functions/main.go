167d01cfc6bfd7849d37c2fdd239d3639e9bd715
Shouvik Bajpayee <bajpayeeshouvik@gmail.com>
2021-09-05 15:44:17 +0530

    go lang commit

--- Using Functions/main.go
+++ Using Functions/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func sayGreeting(n string) {
+	fmt.Printf("Good morning %v\n", n)
+}
+
+func sayBye(n string) {
+	fmt.Printf("Goodbye %v\n", n)
+}
+
+func cycleNames(n []string, f func(string)) {
+	for _, value := range n {
+		f(value)
+	}
+}
+
+func circleArea(r float64) float64 {
+	return math.Pi * r * r
+}
 
 func main() {
-	fmt.Println("Using Functions")
+
+	sayGreeting("Mario")
+	sayGreeting("Luigi")
+	sayBye("Mario")
+	sayBye("Luigi")
+
+	cycleNames([]string{"cloud", "tifa", "barret"}, sayGreeting)
+	cycleNames([]string{"cloud", "tifa", "barret"}, sayBye)
+
+	a1 := circleArea(10.5)
+	a2 := circleArea(15)
+	fmt.Printf("%0.2f %0.3f \n", a1, a2)
+
 }

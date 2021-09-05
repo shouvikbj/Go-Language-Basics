d4b887856cb2a53c1b7f8cfc3a32fb8b8e5f869d
Shouvik Bajpayee <bajpayeeshouvik@gmail.com>
2021-09-05 16:18:33 +0530

    go lang commit

---
+++ Package Scope/greetings.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var points = []int{20, 90, 100, 45, 70}
+
+func sayHello(n string) {
+	fmt.Println("hello", n)
+}
+
+func showScore() {
+	fmt.Printf("You scored %0.2f points.\n", score)
+}
---
+++ Package Scope/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+var score = 99.5
+
+func main() {
+	sayHello("Mario")
+
+	for _, value := range points {
+		fmt.Println(value)
+	}
+
+	showScore()
+}
